04493bbba037f958c67e0e746b888da50b1f1705
Han Khuu <hankhuu@intuitionbrandmarketing.com>
2018-09-25 15:12:35 -0400

    displaying company names

--- companies.js
+++ companies.js
@@ -1,10 +1,9 @@
 app.factory('companies', ['$http', function($http) {
-	return
-	$http.get('https://perspectivemagazine.com/wp-json/wp/v2/categories?per_page=50&page=1')
+	return $http.get('https://perspectivemagazine.com/wp-json/wp/v2/categories?per_page=50&page=1')
 	.success(function(data) {
 		return data;
 	})
-	.error(function(data) {
-		return data;
+	.error(function(err) {
+		return err;
 	});
 }]);
