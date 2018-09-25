e37aa91ea830b0562e036bfa800b2ab4b27d9300
Han Khuu <hankhuu@intuitionbrandmarketing.com>
2018-09-25 13:37:03 -0400

    added angularjs cdn

--- index.js
+++ index.js
@@ -20,11 +20,7 @@ listCat.init = function() {
 };
 listCat.init();
 
-// Using WP api 'posts' to show posts by category company name
-const apiPost = 'https://perspectivemagazine.com/wp-json',
-	  postContainer = $()
-
-
+// Loop through categories
 listCat.render = function( categories ) {
 
 	for ( let category of categories ) {
@@ -77,5 +73,62 @@ listCat.clearCat = function() {
 		$(articleContainer).html('');
 
 };
+
+
+// Using WP api 'posts' to show posts by category company name
+// const apiPost = 'https://perspectivemagazine.com/wp-json',
+// 	  postContainer = $('main#main2');
+// let listPost = {};
+
+// listPost.init = function() {
+
+// 	$.ajax({
+// 		url: apiPost + '/wp/v2/posts?categories=1186',
+// 		success: function ( posts ) {
+// 			listPost.render( posts );
+// 		},
+// 		error: function( err ) {
+// 			console.log( 'Error: ', err );
+// 		}
+// 	});
+// };
+// listPost.init();
+
+// Loop through posts
+// listPost.render = function( posts ) {
+
+// 	for ( let post of posts ) {
+// 		listPost.renderPost( post );
+// 	}
+
+// };
+
+// listPost.renderPost = function( post ) {
+
+//   const articlePost = $( '<article>' ),
+// 			titlePostEl = listPost.getTitleMarkup( post ),
+// 			// contentEl = listPost.getContentMarkup( post );
+
+// 	$( articlePost ).addClass('post');
+// 	$( articlePost ).append( titlePostEl );
+// 	// $( article ).append( contentEl );
+// 	// $( articleContainer ).append( articlePost );
+
+// };
+
+// listPost.getTitleMarkup = function( post ) {
+
+// 	const titlePost = $( '<h2>' ),
+// 		  titlePostLink = $( '<a>' );
+
+// 	$(titlePost).addClass('entry-title');
+// 	$(titlePostLink).text( post.title.rendered );
+// 	// $(titlePostLink).attr( 'href', 'https://perspectivemagazine.com/wp-json/wp/v2/posts?categories=' + post.id );
+
+// 	$(titlePost).append( titlePostLink );
+
+// 	return titlePost;
+
+// };
 
 });
