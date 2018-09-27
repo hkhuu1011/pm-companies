99f3b98484c9340f65e190fd87915f6c76bf5def
Han Khuu <hankhuu@intuitionbrandmarketing.com>
2018-09-27 10:51:07 -0400

    removed HomeController and posts service from index.html

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -4,7 +4,7 @@ app.controller('HomeController', ['$scope', 'companies', function($scope, compan
 	});
 }]);
 
-app.controller('CompanyController', ['$scope', 'companies', '$routeParams', '$http', function($scope, companies, $routeParams, $http) {
+app.controller('CompanyController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
 	$http.get('https://perspectivemagazine.com/wp-json/wp/v2/posts?categories=' + $routeParams.id).success(function(data) {
 		$scope.posts = data;
 		console.log('posts', $scope.posts);
