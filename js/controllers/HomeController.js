61dc9e423b5cb4a712ce70ec252ad07a8b3eca32
Han Khuu <hankhuu@intuitionbrandmarketing.com>
2018-10-03 11:54:54 -0400

    cleaned up HomeController

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -4,19 +4,3 @@ app.controller('HomeController', ['$scope', 'companies', function($scope, compan
 		console.log('companies', $scope.companies);
 	});
 }]);
-
-app.controller('CompanyController', ['$scope', '$routeParams', '$http', 'articles', function($scope, $routeParams, $http, articles) {
-	articles.success(function(data) {
-		$scope.articles = data;
-		console.log('articles', $scope.articles);
-	})
-
-}]);
-
-app.controller('ArticleController', ['$scope', '$routeParams', '$http', 'articles', function($scope, $routeParams, $http, articles) {
-	articles.success(function(data) {
-		$scope.post = data[$routeParams.id];
-		console.log('posts', $scope.post);
-	})
-
-}]);
