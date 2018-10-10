62013463b34a02d05a4f1419d14e8b93ee8434a0
Han Khuu <hankhuu@intuitionbrandmarketing.com>
2018-10-10 12:12:11 -0400

    added scrollTo to fix page loadbug

--- js/controllers/ArticleController.js
+++ js/controllers/ArticleController.js
@@ -1,6 +1,7 @@
 app.controller('ArticleController', ['$scope', '$routeParams', 'articles', function($scope, $routeParams, articles) {
 	articles.success(function(data) {
 		$scope.post = data[$routeParams.id];
+		window.scrollTo(0,0);
 		console.log('posts', $scope.post);
 	});
 }]);
