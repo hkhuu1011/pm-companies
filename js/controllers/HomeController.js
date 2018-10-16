4768cc7441cdc6bcd89157a13bc33a3d482b6ce8
Han Khuu <hankhuu@intuitionbrandmarketing.com>
2018-10-16 14:22:45 -0400

    not best practice - but pulling in all 5 pages

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,10 +1,28 @@
 app.controller('HomeController', ['$scope', 'companies', function($scope, companies) {
+
 	$scope.reloadPage = function() {
 		window.location.reload();
 	};
-	companies.success(function(data) {
-		$scope.companies = data;
-		console.log('companies', $scope.companies);
 
+	companies.getCompanies(100, 1).then(function(companies) {
+	    $scope.companies = companies;
 	});
+
+	companies.getCompanies(100, 2).then(function(companies) {
+	    $scope.companies2 = companies;
+	});
+
+	companies.getCompanies(100, 3).then(function(companies) {
+	    $scope.companies3 = companies;
+	});
+
+	companies.getCompanies(100, 4).then(function(companies) {
+	    $scope.companies4 = companies;
+	});
+
+	companies.getCompanies(100, 5).then(function(companies) {
+	    $scope.companies5 = companies;
+	});
+
 }]);
+
