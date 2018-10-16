54f626cf5a40145a95bebe3d5f1cf7c54006ef31
Han Khuu <hankhuu@intuitionbrandmarketing.com>
2018-10-16 14:21:29 -0400

    refactored http.get

--- js/services/companies.js
+++ js/services/companies.js
@@ -1,9 +1,21 @@
 app.factory('companies', ['$http', function($http) {
-	return $http.get('https://perspectivemagazine.com/wp-json/wp/v2/categories?per_page=50&page=1')
-	.success(function(data) {
-		return data;
-	})
-	.error(function(err) {
-		return err;
-	});
+
+    function fnGetCompanies(itemsPerPage, page) {
+        var ipp = itemsPerPage || 100;
+        var page = page || 1;
+        return $http
+            .get('https://perspectivemagazine.com/wp-json/wp/v2/categories?per_page=' + ipp + '&page=' + page)
+            .then(
+                function(response) {
+                    return response.data;
+                },
+                function(err) {
+                    return err;
+                }
+            );
+    }
+    return {
+        getCompanies: fnGetCompanies
+    }
 }]);
+
